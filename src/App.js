688817555acf38b2e1ca23d5e4b4defaa67b5d09
Phong Bùi <buitrongphonghp@gmail.com>
2022-09-12 10:03:34 +0700

    create header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/atoms/header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routes from "./config";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        {routes.map((route) => {
+          return (
+            <Route
+              path={route.path}
+              component={route.component}
+              key={route.key}
+            />
+          );
+        })}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
