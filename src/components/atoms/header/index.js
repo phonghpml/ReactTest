d8e236521890542668639214139c972081b29581
Phong Bùi <buitrongphonghp@gmail.com>
2022-11-15 18:33:29 +0700

    update code

--- src/components/atoms/header/index.js
+++ src/components/atoms/header/index.js
@@ -1,36 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => {
-  const listHeader = ["Home", "About", "Contact"];
-
-  return (
-    <div className="bg-yellow-600 py-4">
-      <div className="container mx-auto">
-        <div className="flex justify-between">
-          <div className="flex">
-            {listHeader.map((item) => {
-              return (
-                <Link to={`/${item}`} key={item}>
-                  <div className="px-4">
-                    <p className="text-white capitalize">{item}</p>
-                  </div>
-                </Link>
-              );
-            })}
-          </div>
-          <div className="flex">
-            <Link to={`/Login`} className="px-4 text-white capitalize">
-              Login
-            </Link>
-            <Link to={`/Logout`} className="px-4 text-white capitalize">
-              Logout
-            </Link>
+class Header extends React.Component  {
+  listHeader = ["Home", "About", "Contact"];
+  render(){
+    return (
+      <div className="bg-yellow-600 py-4">
+        <div className="container mx-auto">
+          <div className="flex justify-between">
+            <div className="flex">
+              {listHeader.map((item) => {
+                return (
+                  <Link to={`/${item}`} key={item}>
+                    <div className="px-4">
+                      <p className="text-white capitalize">{item}</p>
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
+            <div className="flex">
+              <Link to={`/Login`}  className="px-4 text-white capitalize">
+                Login
+              </Link>
+              <Link to={`/Logout`} className="px-4 text-white capitalize">
+                Logout
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Header;
--- src/components/atoms/login/index.js
+++ src/components/atoms/login/index.js
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-  let username, password;
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [dataSubmit, setDataSubmit] = useState();
+
+  // const callApi = async () => {
+  //   if (localStorage.getItem("jwt_token")) {
+  //     const data = await fetch("api", {
+  //       mode: "cors",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "Access-Control-Allow-Origin": "*",
+  //         authen: localStorage.getItem("jwt_token"),
+  //       },
+  //     });
+  //   } else {
+  //     fetch("api", {
+  //       mode: "cors",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "Access-Control-Allow-Origin": "*",
+  //       },
+  //     });
+  //   }
+  // };
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (dataSubmit) {
+      console.log(dataSubmit.username);
+      fetch("http://localhost:2000/auth/login", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          username: dataSubmit.username,
+          password: dataSubmit.password,
+        }),
+      }).then(async (data) => {
+        const _data = await data.json();
+        if (_data.status === "success") {
+          localStorage.setItem("jwt_token", _data?.data?.access_token);
+          navigate("/Home");
+        }
+      });
+    }
+  }, [dataSubmit, navigate]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    console.log(username);
-    console.log(password);
+    console.log(setDataSubmit({ username, password }));
   };
 
   return (
-    <div className="container mx-auto">
-      <form
+    <form className="container mx-auto">
+      <div
         className="bg-slate-500 w-1/5 h-1/3 mx-auto my-5 min-w-max text-gray-700"
-        onSubmit={handleSubmit}
+        // onSubmit={() => handleSubmit(e)}
       >
         <div className="flex justify-around p-4 m-2">
           <label className="m-2">Username</label>
@@ -23,7 +70,7 @@ const Login = () => {
             className="rounded"
             required
             onChange={(e) => {
-              username = e.target.value;
+              setUsername(e.target.value);
             }}
           ></input>
         </div>
@@ -35,16 +82,22 @@ const Login = () => {
             className="rounded"
             required
             onChange={(e) => {
-              password = e.target.value;
+              setPassword(e.target.value);
             }}
           ></input>
         </div>
 
         <div className="flex justify-around p-4">
-          <input type="submit" className="bg-white rounded p-1"></input>
+          <button
+            // type="submit"
+            // className="bg-white rounded p-1"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };
 
